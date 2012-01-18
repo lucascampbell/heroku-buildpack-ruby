55118044737160fadb16344f191973148a1fc534
lucas campbell-rossen <lucas@rhomobile.com>
2012-01-18 11:10:03 -0800

    new

--- lib/language_pack/rhoconnect.rb
+++ lib/language_pack/rhoconnect.rb
@@ -25,7 +25,8 @@ class LanguagePack::Rhoconnect < LanguagePack::Ruby
 
     puts "starting to extract resource..."
     system "tar -xmvf #{RESOURCE_TAR_FILE}"
-    system "mv #{RESOURCE_FILE} /app/"
+    system "cp -r #{RESOURCE_FILE} /app/"
+    system "ls -ltr /app/"
     File.delete RESOURCE_TAR_FILE
 
     puts "-----> Finished extracting resource"
