f83a45fb44fb80e0522a55efeb9e6b6019d601de
lucas campbell-rossen <lucas@rhomobile.com>
2012-01-18 13:58:09 -0800

    new

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -378,7 +378,7 @@ params = CGI.parse(uri.query || "")
   # @note it sets a flag, so the path can only be loaded once
   def add_bundler_to_load_path
     return if @bundler_loadpath
-    #$: << File.expand_path(Dir["#{slug_vendor_base}/gems/bundler*/lib"].first)
+    $: << File.expand_path(Dir["#{slug_vendor_base}/gems/bundler*/lib"].first)
     @bundler_loadpath = true
   end
 
