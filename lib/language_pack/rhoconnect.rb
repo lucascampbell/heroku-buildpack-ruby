d4a86fdbb80dfe4dc8a02398a7f8fc63537be276
lucas campbell-rossen <lucas@rhomobile.com>
2012-01-23 16:16:37 -0800

    cp after super

--- lib/language_pack/rhoconnect.rb
+++ lib/language_pack/rhoconnect.rb
@@ -36,6 +36,7 @@ class LanguagePack::Rhoconnect < LanguagePack::Rack
     puts "-----> Finished extracting resource"
     
     super
+    system "cp -r /app/#{RESOURCE_FILE}/* /app/"
   end
   
 end
